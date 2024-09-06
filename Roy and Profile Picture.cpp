1657c152eac9e8114d3d7a56c8413096a9c78ef0
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-05 23:11:09 -0400

    Add project files.

---
+++ Roy and Profile Picture.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int L, N;
+
+    // قراءة قيمة L
+    cin >> L;
+
+    // قراءة عدد الصور N
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int W, H;
+        cin >> W >> H;  // قراءة أبعاد الصورة
+
+        if (W < L || H < L) {
+            cout << "UPLOAD ANOTHER" << endl;
+        }
+        else if (W == H) {
+            cout << "ACCEPTED" << endl;
+        }
+        else {
+            cout << "CROP IT" << endl;
+        }
+    }
+
+    return 0;
+}
